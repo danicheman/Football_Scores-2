f8e680fb8f1ae65266b8a4747be01095ec93ea5a
nick <nostrowski79@gmail.com>
2015-10-25 15:35:10 -0400

    Creating a simple widget to learn!

---
+++ app/src/main/java/barqsoft/footballscores/widget/ShortcutWidget.java
@@ -0,0 +1,26 @@
+package barqsoft.footballscores.widget;
+
+import android.annotation.TargetApi;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.os.Build;
+import android.widget.RemoteViews;
+
+import barqsoft.footballscores.R;
+
+/**
+ * Created by nick on 10/25/15.
+ */
+public class ShortcutWidget  extends AppWidgetProvider{
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1)
+    private void setRemoteContentDescription(RemoteViews views, String description) {
+        views.setContentDescription(R.id.widget_icon, description);
+    }
+}
